6f998ab1ccc58b1df13c82b52c07feb1c661b6cf
Gayus Jones Petra <jonespetra102@gmail.com>
2026-02-26 09:28:48 +0700

    .

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -46,7 +46,7 @@ fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("API telah berjalan. Dibuat oleh Abdullah Ubaid.")
+            call.respondText("API telah berjalan. Dibuat oleh Gayus Jones Petra.")
         }
 
         // Route Plants
